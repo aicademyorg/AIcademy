40283f961104b2b7836c4d69faf064b6ebf8196a
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-05-27 01:24:32 +0530

    Update layout.tsx

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,16 +1,16 @@
-import type { ReactNode } from 'react';
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions, linkItems } from '@/app/layout.config';
+import type { ReactNode } from "react";
+import { HomeLayout } from "fumadocs-ui/layouts/home";
+import { baseOptions, linkItems } from "@/app/layout.config";
 import {
   NavbarMenu,
   NavbarMenuContent,
   NavbarMenuLink,
   NavbarMenuTrigger,
-} from 'fumadocs-ui/layouts/home/navbar';
-import Link from 'fumadocs-core/link';
-import Image from 'next/image';
-import Preview from '@/public/banner.png';
-import { Book, ComponentIcon, Pencil, PlusIcon, Server } from 'lucide-react';
+} from "fumadocs-ui/layouts/home/navbar";
+import Link from "fumadocs-core/link";
+import Image from "next/image";
+import Preview from "@/public/banner.png";
+import { Book, ComponentIcon, Pencil, PlusIcon, Server } from "lucide-react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -18,25 +18,25 @@ export default function Layout({ children }: { children: ReactNode }) {
       {...baseOptions}
       links={[
         {
-          type: 'menu',
-          on: 'menu',
-          text: 'Courses',
+          type: "menu",
+          on: "menu",
+          text: "Courses",
           items: [
             {
-              text: 'Getting Started',
-              url: '/docs/ui',
+              text: "Getting Started",
+              url: "/docs/ui",
               icon: <Book />,
             },
             {
-              text: 'Components',
-              url: '/docs/ui/components',
+              text: "Components",
+              url: "/docs/ui/components",
               icon: <ComponentIcon />,
             },
           ],
         },
         {
-          type: 'custom',
-          on: 'nav',
+          type: "custom",
+          on: "nav",
           children: (
             <NavbarMenu>
               <NavbarMenuTrigger>
@@ -51,7 +51,7 @@ export default function Layout({ children }: { children: ReactNode }) {
                       className="rounded-t-lg object-cover"
                       style={{
                         maskImage:
-                          'linear-gradient(to bottom,white 60%,transparent)',
+                          "linear-gradient(to bottom,white 60%,transparent)",
                       }}
                     />
                   </div>
@@ -89,9 +89,7 @@ export default function Layout({ children }: { children: ReactNode }) {
                 >
                   <Pencil className="bg-fd-primary text-fd-primary-foreground p-1 mb-2 rounded-md" />
                   <p className="font-medium">Bash 101</p>
-                  <p className="text-fd-muted-foreground text-sm">
-                    Learn Bash
-                  </p>
+                  <p className="text-fd-muted-foreground text-sm">Learn Bash</p>
                 </NavbarMenuLink>
 
                 <NavbarMenuLink
@@ -100,9 +98,7 @@ export default function Layout({ children }: { children: ReactNode }) {
                 >
                   <PlusIcon className="bg-fd-primary text-fd-primary-foreground p-1 mb-2 rounded-md" />
                   <p className="font-medium">LLMs 101</p>
-                  <p className="text-fd-muted-foreground text-sm">
-                    Learn LLMs
-                  </p>
+                  <p className="text-fd-muted-foreground text-sm">Learn LLMs</p>
                 </NavbarMenuLink>
               </NavbarMenuContent>
             </NavbarMenu>
@@ -113,30 +109,6 @@ export default function Layout({ children }: { children: ReactNode }) {
       className="dark:bg-neutral-950 dark:[--color-fd-background:var(--color-neutral-950)]"
     >
       {children}
-      <Footer />
     </HomeLayout>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="mt-auto border-t bg-fd-card py-12 text-fd-secondary-foreground">
-      <div className="container flex flex-col gap-4 sm:flex-row sm:items-end sm:justify-between">
-        <div>
-          <p className="mb-1 text-sm font-semibold">AIcademy</p>
-          <p className="text-xs">
-            Built with ❤️ by{' '}
-            <a
-              href="https://github.com/3p5ilon"
-              rel="noreferrer noopener"
-              target="_blank"
-              className="font-medium"
-            >
-              Ɛpsilon
-            </a>
-          </p>
-        </div>
-      </div>
-    </footer>
   );
 }
