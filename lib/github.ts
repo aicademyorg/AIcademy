ec542f04a1d4b1568def2857dc806209a719ca3b
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-05-26 19:21:30 +0530

    Add github app for feedback

--- lib/github.ts
+++ lib/github.ts
@@ -1,23 +1,40 @@
-import { Octokit } from 'octokit';
-import type { Feedback } from '@/components/rate';
+import { App, Octokit } from "octokit";
+import type { Feedback } from "@/components/rate";
 
-export const repo = 'AIcademy';
-export const owner = 'aicademyorg';
-export const DocsCategory = 'Feedback';
+export const repo = "AIcademy";
+export const owner = "aicademyorg";
+export const DocsCategory = "Feedback";
 
 let instance: Octokit | undefined;
 
 async function getOctokit(): Promise<Octokit> {
   if (instance) return instance;
-  const token = process.env.GITHUB_TOKEN;
+  const appId = process.env.GITHUB_APP_ID;
+  const privateKey = process.env.GITHUB_APP_PRIVATE_KEY;
 
-  if (!token) {
+  if (!appId || !privateKey) {
     throw new Error(
-      'No GitHub token provided, docs feedback feature will not work.',
+      "No GitHub keys provided for Github app, docs feedback feature will not work."
     );
   }
 
-  instance = new Octokit({ auth: token });
+  const app = new App({
+    appId,
+    privateKey,
+  });
+
+  const { data } = await app.octokit.request(
+    "GET /repos/{owner}/{repo}/installation",
+    {
+      owner,
+      repo,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
+
+  instance = await app.getInstallationOctokit(data.id);
   return instance;
 }
 
@@ -55,18 +72,18 @@ async function getFeedbackDestination() {
 }
 
 export async function onRateAction(url: string, feedback: Feedback) {
-  'use server';
+  "use server";
   const octokit = await getOctokit();
   const destination = await getFeedbackDestination();
   if (!octokit || !destination) return;
 
   const category = destination.discussionCategories.nodes.find(
-    (category) => category.name === DocsCategory,
+    (category) => category.name === DocsCategory
   );
 
   if (!category)
     throw new Error(
-      `Please create a "${DocsCategory}" category in GitHub Discussion`,
+      `Please create a "${DocsCategory}" category in GitHub Discussion`
     );
 
   const title = `Feedback for ${url}`;
@@ -80,7 +97,9 @@ export async function onRateAction(url: string, feedback: Feedback) {
     };
   } = await octokit.graphql(`
     query {
-      search(type: DISCUSSION, query: ${JSON.stringify(`${title} in:title repo:${owner}/${repo} author:@me`)}, first: 1) {
+      search(type: DISCUSSION, query: ${JSON.stringify(
+        `${title} in:title repo:${owner}/${repo} author:@me`
+      )}, first: 1) {
         nodes {
           ... on Discussion { id }
         }
@@ -90,14 +109,20 @@ export async function onRateAction(url: string, feedback: Feedback) {
   if (discussions.length > 0) {
     await octokit.graphql(`
       mutation {
-        addDiscussionComment(input: { body: ${JSON.stringify(body)}, discussionId: "${discussions[0].id}" }) {
+        addDiscussionComment(input: { body: ${JSON.stringify(
+          body
+        )}, discussionId: "${discussions[0].id}" }) {
           comment { id }
         }
       }`);
   } else {
     await octokit.graphql(`
       mutation {
-        createDiscussion(input: { repositoryId: "${destination.id}", categoryId: "${category.id}", body: ${JSON.stringify(body)}, title: ${JSON.stringify(title)} }) {
+        createDiscussion(input: { repositoryId: "${
+          destination.id
+        }", categoryId: "${category.id}", body: ${JSON.stringify(
+      body
+    )}, title: ${JSON.stringify(title)} }) {
           discussion { id }
         }
       }`);
